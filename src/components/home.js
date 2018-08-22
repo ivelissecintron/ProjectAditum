d450508121663d3196dfc7648be8b833e4e73856
Ivelisse <msivelisse@gmail.com>
2018-08-22 12:11:03 -0500

    implemented user authentication

---
+++ src/components/home.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+
+class Home extends Component {
+    constructor() {
+        super()
+    }
+
+
+    render() {
+        const imageStyle = {
+            width: 400
+        }
+        return (
+            <div>
+                
+            </div>
+        )
+
+    }
+}
+
+export default Home
